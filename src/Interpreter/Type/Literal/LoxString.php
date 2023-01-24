28144a4bce7eaa1980211a77138429e4173811b3
Vincent van Dijk <info@vincentvandijk.com>
2023-01-24 15:37:01 +0100

    Added return types to closures and added regular expression match function to LoxString.

--- src/Interpreter/Type/Literal/LoxString.php
+++ src/Interpreter/Type/Literal/LoxString.php
@@ -4,6 +4,7 @@ namespace ExtendsSoftware\LoxPHP\Interpreter\Type\Literal;
 
 use function array_merge;
 use function explode;
+use function preg_match;
 use function str_replace;
 use function strlen;
 use function strrev;
@@ -18,8 +19,8 @@ class LoxString extends LoxLiteral
         return array_merge(
             parent::getFunctions(),
             [
-                'length' => fn() => new LoxNumber(strlen($this->value)),
-                'get' => function (string $index = '0') {
+                'length' => fn(): LoxNumber => new LoxNumber(strlen($this->value)),
+                'get' => function (string $index = '0'): LoxString|LoxNil {
                     $index = (int)$index;
                     if ($index < 0) {
                         $index = strlen($this->value) - abs($index);
@@ -31,18 +32,26 @@ class LoxString extends LoxLiteral
 
                     return new LoxNil();
                 },
-                'replace' => fn(string $search, string $replace) => new LoxString(
+                'replace' => fn(string $search, string $replace): LoxString => new LoxString(
                     str_replace($search, $replace, $this->value)
                 ),
-                'explode' => function (string $separator = ' ') {
+                'explode' => function (string $separator = ' '): LoxArray {
                     if (strlen($separator) === 0) {
                         $separator = ' ';
                     }
 
                     return new LoxArray(explode($separator, $this->value));
                 },
-                'trim' => fn() => new LoxString(trim($this->value)),
-                'reverse' => fn() => new LoxString(strrev($this->value)),
+                'trim' => fn(): LoxString => new LoxString(trim($this->value)),
+                'reverse' => fn(): LoxString => new LoxString(strrev($this->value)),
+                'match' => function (string $pattern): LoxNil|LoxArray {
+                    $result = @preg_match($pattern, $this->value, $matches);
+                    if ($result === false) {
+                        return new LoxNil();
+                    }
+
+                    return new LoxArray($matches);
+                },
             ]
         );
     }

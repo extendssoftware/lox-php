049909a6a52c1367eae63a21e585bfcd92f52aa8
Vincent van Dijk <info@vincentvandijk.com>
2023-01-24 10:47:13 +0100

    Added array functions count, get, pop, push, shift and unshift.

--- src/Interpreter/Type/Literal/LoxArray.php
+++ src/Interpreter/Type/Literal/LoxArray.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace ExtendsSoftware\LoxPHP\Interpreter\Type\Literal;
 
 use function array_merge;
+use function array_pop;
+use function array_push;
+use function array_shift;
+use function array_unshift;
 use function implode;
 
 class LoxArray extends LoxLiteral
@@ -25,6 +29,30 @@ class LoxArray extends LoxLiteral
             parent::getFunctions(),
             [
                 'count' => fn() => new LoxNumber(count($this->value)),
+                'get' => function ($index) {
+                    $index = (int)(string)$index;
+                    if ($index < 0) {
+                        $index = count($this->value) - abs($index);
+                    }
+
+                    return $this->value[$index] ?? new LoxNil();
+                },
+                'pop' => fn() => array_pop($this->value),
+                'push' => function ($value) {
+                    if ($value === $this) {
+                        return new LoxNil();
+                    }
+
+                    return new LoxNumber(array_push($this->value, $value));
+                },
+                'shift' => fn() => array_shift($this->value),
+                'unshift' => function ($value) {
+                    if ($value === $this) {
+                        return new LoxNil();
+                    }
+
+                    return array_unshift($this->value, $value);
+                },
             ]
         );
     }

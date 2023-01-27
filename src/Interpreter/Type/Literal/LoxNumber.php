c5e8a46fdd1f4da165b91f41c122c850e8eba8fb
Vincent van Dijk <info@vincentvandijk.com>
2023-01-27 19:30:11 +0100

    Added number abs, ceil, floor and round function.

--- src/Interpreter/Type/Literal/LoxNumber.php
+++ src/Interpreter/Type/Literal/LoxNumber.php
@@ -2,6 +2,26 @@
 
 namespace ExtendsSoftware\LoxPHP\Interpreter\Type\Literal;
 
+use ExtendsSoftware\LoxPHP\Interpreter\Type\Literal\Function\Number\Abs;
+use ExtendsSoftware\LoxPHP\Interpreter\Type\Literal\Function\Number\Ceil;
+use ExtendsSoftware\LoxPHP\Interpreter\Type\Literal\Function\Number\Floor;
+use ExtendsSoftware\LoxPHP\Interpreter\Type\Literal\Function\Number\Round;
+use ExtendsSoftware\LoxPHP\Scanner\Token\TokenInterface;
+
 class LoxNumber extends LoxLiteral
 {
+    /**
+     * @inheritDoc
+     */
+    public function get(TokenInterface $name): mixed
+    {
+        $value = $this->value;
+        return match ($name->getLexeme()) {
+            'abs' => new Abs($value),
+            'ceil' => new Ceil($value),
+            'floor' => new Floor($value),
+            'round' => new Round($value),
+            default => parent::get($name),
+        };
+    }
 }
